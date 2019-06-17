b7c8f932969efa2dbb37f53738d7fae0b15fe17b
debbysa <deby.silvia23@gmail.com>
2019-06-17 21:40:38 +0700

    menambahkan require config database

--- Server.js
+++ Server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const server = require("http").Server(app);
+require("./config/database");
 
 app.use(express.json());
 
-server.listen(3000, function() {
+app.listen(3000, function() {
   console.log("server running at localhost:3000");
 });
