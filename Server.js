50655ab6965d4d6d5f72101298f86bc4ce5990c5
debbysa <deby.silvia23@gmail.com>
2019-06-20 11:57:34 +0700

    menambahkan route /history pada app.use

--- Server.js
+++ Server.js
@@ -3,6 +3,7 @@ const app = express();
 require("./config/database");
 
 app.use(express.json());
+app.use("/history", require("./router/historyRouter"));
 
 app.listen(3000, function() {
   console.log("server running at localhost:3000");
