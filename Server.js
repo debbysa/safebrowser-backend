f351789206a4d6e724145cd220b520540c6ea7fb
debbysa <deby.silvia23@gmail.com>
2019-07-02 21:01:19 +0700

    menambahkan cors untuk deploy heroku karena portnya nanti akan berbeda

--- Server.js
+++ Server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("./config/database");
 
 app.use(express.json());
+
+app.use(cors());
+
 app.use("/history", require("./router/historyRouter"));
 
-app.listen(3000, function() {
-  console.log("server running at localhost:3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("server running on port 3000");
 });
