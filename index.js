e8f22302be929f379c255fce8b90abea1fb2d56d
chintyadewi <chintyadewi437@gmail.com>
2019-07-06 21:02:45 +0700

    menambah route & halaman ketika url di blokir

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 require("./config/database");
 
 app.use(express.json());
 
 app.use(cors());
 
+// Using EJS template engine
+app.set("", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static("public"));
+
+app.use("/", require("./router/webPage403Router"));
 app.use("/history", require("./router/historyRouter"));
 
 app.listen(process.env.PORT || 3000, function() {
