4e029c87c1afe187f52076b09df82d93eada0dcd
debbysa <deby.silvia23@gmail.com>
2019-09-13 21:49:41 +0700

    menambahkan rute select history berdasarkan plugincode

--- router/historyRouter.js
+++ router/historyRouter.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const historyController = require("../controllers/historyController");
 
 router.get("/", historyController.index);
+router.get("/:pluginCode", historyController.details);
 router.post("/", historyController.store);
+router.put("/:pluginCode", historyController.update);
+router.delete("/:pluginCode", historyController.delete);
 
 module.exports = router;
